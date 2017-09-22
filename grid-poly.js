524ecb99b8d4b2fefe1a2a9e305d73e5258491f2
Ronn Ross <ronn.ross@gmail.com>
2017-09-22 10:39:28 -0400

    starting on fallback

---
+++ grid-poly.js
@@ -0,0 +1,10 @@
+(function() {
+  let gridParents = document.querySelectorAll(".grid");
+  console.log(gridParents);
+  gridParents.forEach(el => {
+    // el.classList.add("red")
+    console.log('chilren', el.children)
+  })  
+
+
+})();
