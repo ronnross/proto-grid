2016301c087111e233637b62393bbceb13e752be
Ronn Ross <ronn.ross@gmail.com>
2017-10-17 08:42:06 -0400

    Updated poly code from working session

--- src/grid-poly.js
+++ src/grid-poly.js
@@ -4,27 +4,34 @@
   }
 
   const gridNodes = document.querySelectorAll(".grid");
-  const gridEls = Array.prototype.slice.call(gridNodes);
+  const grids = Array.prototype.slice.call(gridNodes);
 
-  gridEls.forEach(function(el) {
+  const rSpan = /^span (\d?\d)$/;
+
+  grids.forEach(function(el) {
     const childArray = Array.prototype.slice.call(el.children);
-    let offSet = 0;
-    childArray.forEach(function(ch, idx) {
-      let elList = ch.classList;
-      if (elList.contains("span-2")) {
-        elList.add("red-border");
-        elList.add("ms-col-" + indexInc(idx + offSet));
-        elList.add("span-2");
-        offSet += 1;
-      } else if (elList.contains("span-3-md")) {
-        elList.add("blue-border");
-        elList.add("ms-col-" + indexInc(idx + offSet));
-        elList.add("span-2");
-        offSet += 2;
-      } else {
-        elList.add("red-border");
-        elList.add("ms-col-" + indexInc(idx + offSet));
-      }
-    });
+    let currCol = 0;
+
+    for (let i = 0; i < childArray.length; i++) {
+      const gi = el.children[i];
+      let s = window.getComputedStyle(gi);
+      if (s.display === "none") continue;
+
+      let span = parseInt(s.msGridColumnSpan) || 1;
+      console.log("span!!!", span, s.msGridColumnSpan);
+      /*const match = rSpan.exec(s.gridColumnStart);
+      if (match) {
+        span = Number(match[1]);
+      }*/
+      // console.log(gi.className, s.gridColumnStart)
+      // console.log('s', s)
+      console.log("span", s.msGridColumnSpan);
+      //let list = el.children[i].classList;
+      //let span = calculateColSpan(list);
+      let start = currCol + span > 12 ? 0 : currCol;
+      log({ span: span, start: start, currCol: currCol });
+      gi.classList.add("ms-col-" + (start + 1));
+      currCol = start + span;
+    }
   });
 })();
